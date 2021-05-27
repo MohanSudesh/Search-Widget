babe1ad3b2ccf85fb2ee24d9e13114107ad0284f
Sudesh <mohansudesh@gmail.com>
2021-05-27 17:07:27 +0530

    font changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/search-widget-api", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://mohansudesh:8328473594@task-manager.2chc0.mongodb.net/task-manager?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 mongoose.connection.once("open", () => {
   console.log("DataBase Connected");
