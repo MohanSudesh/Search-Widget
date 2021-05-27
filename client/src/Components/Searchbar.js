deebaaa4a9d6a22fbf72193f827028b7ae8323e4
Sudesh <mohansudesh@gmail.com>
2021-05-27 17:06:26 +0530

    fonts

--- client/src/Components/Searchbar.js
+++ client/src/Components/Searchbar.js
@@ -19,7 +19,7 @@ export default function Searchbar() {
         <div className="row justify-content-center align-items-center ">
           <div className="col-md-8 col-xs-2">
             <div className="input-group">
-              <span>
+              {/* <span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -30,7 +30,7 @@ export default function Searchbar() {
                 >
                   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                 </svg>
-              </span>
+              </span> */}
               <input
                 type="text"
                 className="form-control input-field rounded-pill input-text"
