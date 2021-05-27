49126f9c269a18cebade7ecafc3a4d3df7563996
Sudesh <mohansudesh@gmail.com>
2021-05-27 16:38:10 +0530

    added font and font size

--- client/src/Components/Searchbar.js
+++ client/src/Components/Searchbar.js
@@ -19,9 +19,21 @@ export default function Searchbar() {
         <div className="row justify-content-center align-items-center ">
           <div className="col-md-8 col-xs-2">
             <div className="input-group">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-search"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
+              </span>
               <input
                 type="text"
-                className="form-control input-field"
+                className="form-control input-field rounded-pill input-text"
                 placeholder="Search for product"
                 onChange={(e) => {
                   setSearchText(e.target.value);
@@ -42,11 +54,13 @@ export default function Searchbar() {
           .map((food, key) => {
             return (
               <div clasName="row align-items-center" key={key}>
-                <div className=" col-md-4 col-xs-2 form-control">
+                <div className="col-md-4 col-xs-2 form-control rounded-lg">
                   <p>
-                    <strong>{food.name}</strong>
+                    <strong className="food-text">{food.name}</strong>
                     <br />
-                    {food.availableAt}
+                    <p className="address-text">
+                      Available at {food.availableAt}
+                    </p>
                   </p>{" "}
                 </div>
               </div>
